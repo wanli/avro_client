76775bb913ef906b8e23345dc7443a4feadea2ad
Wanli Yang <wanli@twitter.com>
2010-09-02 16:22:25 -0700

    add some examples and some tests.

---
+++ test/sample_server.rb
@@ -0,0 +1,72 @@
+require 'socket'
+require 'rubygems'
+require 'avro'
+
+AVRO_PROTOCOL = Avro::Protocol.parse(File.open("#{File.expand_path(File.dirname(__FILE__))}/sample_protocol.json", 'r').read)
+
+class SampleResponder < Avro::IPC::Responder
+  def initialize
+    super(AVRO_PROTOCOL)
+  end
+
+  def call(message, request)
+    raise ArgumentError, "Blank message" if message.name.nil?
+    raise ArgumentError, "Blank request" if request.nil?
+
+    send(message.name.to_sym, request) if self.public_methods.include?(message.name)
+  rescue ArgumentError => e
+    e.inspect
+  end
+
+  def make_sample(request)
+    sample_name = request['sample_name'] || 'default sample name'
+    raise ArgumentError, "Invalid sample count specified" if request['sample_count'] <= 0
+    (1..request['sample_count']).map { |sample_id| { 'name' => sample_name, 'id' => sample_id } }
+  end
+end
+
+class SampleHandler
+  def initialize(address, port)
+    @ip_address = address
+    @port = port
+  end
+
+  def run(&block)
+    server = TCPServer.new(@ip_address, @port)
+    sockets = [server]
+    while true
+      ready = select(sockets)
+      readable = ready[0]
+
+      readable.each do |socket|
+        if socket == server
+          client = server.accept
+          sockets << client
+        else
+          begin
+            handle(socket)
+            yield
+          rescue => e
+            sockets.delete(socket)
+            socket.close
+          end
+        end
+      end
+    end
+  end
+
+  def handle(request)
+    responder = SampleResponder.new()
+    transport = Avro::IPC::SocketTransport.new(request)
+    str = transport.read_framed_message
+    transport.write_framed_message(responder.respond(str))
+  end
+end
+
+if __FILE__ == $0
+  requests_processed = 0
+  SampleHandler.new('localhost', 11011).run {
+    requests_processed += 1
+    puts "Request processed #{requests_processed}"
+  }
+end
---
+++ test/test_simple.rb
@@ -0,0 +1,44 @@
+require 'rubygems'
+require 'socket'
+require "test/unit"
+require 'pp'
+
+require "#{File.dirname(__FILE__)}/../lib/avro_client.rb"
+require "#{File.dirname(__FILE__)}/sample_server.rb"
+
+class TestSampleServer < Test::Unit::TestCase
+  def setup
+    @forked_pid = fork { SampleHandler.new('localhost', 11011).run } 
+    @protocol = Avro::Protocol.parse(File.open("#{File.expand_path(File.dirname(__FILE__))}/sample_protocol.json", 'r').read)
+    @servers = ['localhost:11011'] 
+    @avro = AvroClient.new(@servers, {:protocol => @protocol, :timeout => 5, :raise => true, :retries => 3} )
+  end
+
+  def teardown
+    Process.kill("HUP", @forked_pid)
+    Process.wait
+  end
+
+  def test_simple
+    result = @avro.make_sample('sample_name' => 'Foo', 'sample_count' => 5)
+    assert_equal 5, result.size
+    for sample in result
+      assert_equal 'Foo', sample['name']
+    end 
+    assert_equal 1, result.first['id']
+  end
+
+  def test_default_sample_name
+    result = @avro.make_sample('sample_count' => 3)
+    assert_equal 3, result.size
+    for sample in result
+      assert_equal 'default sample name', sample['name']
+    end 
+    assert_equal 1, result.first['id']
+  end 
+
+  def test_invalid_sample_count
+    result = @avro.make_sample('sample_name' => 'Foo', 'sample_count' => -5)
+    assert_equal "#<ArgumentError: Invalid sample count specified>", result
+  end
+end
